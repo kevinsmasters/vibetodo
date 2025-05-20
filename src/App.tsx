5f4b5d4bdbdea9f243da320956f10c020689f69c
Kevin Masters <irimiuke@gmail.com>
2025-05-20 10:51:42 -0400

    completed added input and passing test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import './App.css'
+import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 
-const todos = [
-  { id: 1, text: 'Buy milk' },
-  { id: 2, text: 'Write code' },
-];
+type Todo = {
+  id: number;
+  text: string;
+};
+/* TODO:combine types used here and in the TodoItem */
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, text: 'Buy milk' },
+    { id: 2, text: 'Write code' },
+  ]);
 
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (!input.trim()) return;
+    setTodos(prev => [
+      ...prev,
+      { id: Date.now(), text: input.trim() },
+    ]);
+    setInput('');
+  }
   return (
     <div>
       <h1>Todo List</h1>
+      <div>
+        <input
+          placeholder="Add a new todo"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
       {todos ? (
         <ul>
         {todos.map(todo => (
